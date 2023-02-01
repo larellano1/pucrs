8dadc1b72eb7d962fbbc4207d06b0d0f1c461117
Luis Arellano <lfv.arellano@gmail.com>
2023-02-01 12:08:46 -0300

    Finalização do código para criação das tabelas.

--- bdr/vendas.py
+++ bdr/vendas.py
@@ -7,7 +7,7 @@ engine = sa.create_engine("sqlite:///vendas.db")
 base = orm.declarative_base()
 
 #Table clientes
-class cliente(base):
+class clientes(base):
     __tablename__ = "clientes"
     cpf = sa.Column(sa.CHAR(14), primary_key=True, index=True)
     nome = sa.Column(sa.VARCHAR(100), nullable=False)
@@ -16,13 +16,20 @@ class cliente(base):
     salario = sa.Column(sa.DECIMAL(10,2))
     dia_mes_aniversario = sa.Column(sa.CHAR(5))
 
-class produto(base):
+class produtos(base):
     __tablename__ = "produtos"
     cod_barras = sa.Column(sa.CHAR(10), primary_key=True, index=True)
     descricao = sa.Column(sa.VARCHAR(100), nullable=False)
 
 class vendas(base):
     __tablename__ = "vendas"
+    id_venda = sa.Column(sa.INTEGER, primary_key=True, index=True)
     dia_venda = sa.Column(sa.DATETIME, default=datetime.date.today())
-    cliente = sa.Column(sa.CHAR(14), sa.ForeignKey("clientes.cpf"),ondelete="NO ACTION", onupdate="CASCADE")
-    produto = sa.Column(sa.CHAR(10), onupdate="CASCADE", ondelete="NO ACTION")
+    cliente = sa.Column(sa.CHAR(14), sa.ForeignKey("clientes.cpf"),onupdate="CASCADE")
+    produto = sa.Column(sa.CHAR(10), sa.ForeignKey("produtos.cod_barras"), onupdate="CASCADE")
+
+try:
+    base.metadata.create_all(engine)
+    print("Tabelas criadas")
+except ValueError:
+    ValueError()
