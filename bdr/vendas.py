62fefb5c6528ee052c585a350bd43a21a183f708
Luis Arellano <lfv.arellano@gmail.com>
2023-02-01 13:35:40 -0300

    Inclusao de função para inserir dados no banco de dados

--- bdr/vendas.py
+++ bdr/vendas.py
@@ -25,11 +25,21 @@ class vendas(base):
     __tablename__ = "vendas"
     id_venda = sa.Column(sa.INTEGER, primary_key=True, index=True)
     dia_venda = sa.Column(sa.DATETIME, default=datetime.date.today())
-    cliente = sa.Column(sa.CHAR(14), sa.ForeignKey("clientes.cpf"),onupdate="CASCADE")
+    cliente = sa.Column(sa.CHAR(14), sa.ForeignKey("clientes.cpf"), onupdate="CASCADE")
     produto = sa.Column(sa.CHAR(10), sa.ForeignKey("produtos.cod_barras"), onupdate="CASCADE")
-
 try:
     base.metadata.create_all(engine)
     print("Tabelas criadas")
 except ValueError:
     ValueError()
+
+#Create a code that inserts a new client in the database
+def insert_client(cpf, nome, email, genero, salario, dia_mes_aniversario):
+    session = orm.sessionmaker(bind=engine)()
+    cliente = clientes(cpf=cpf, nome=nome, email=email, genero=genero, salario=salario, dia_mes_aniversario=dia_mes_aniversario)
+    session.add(cliente)
+    session.commit()
+    session.close()
+
+#Use the function to insert a new client
+insert_client("123.456.789-00", "João da Silva", "joao@gmail.com", "M", 1000.00, "01/01")
